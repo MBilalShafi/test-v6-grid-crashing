0b676cd7423c0d100a8d5f62285644660589a6af
Bilal Shafi <bilalshafidev@gmail.com>
2023-03-07 16:36:48 +0500

    Grid error reproduction

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import { DataGridPremium } from '@mui/x-data-grid-premium';
 import './App.css';
+
+const rows = [
+  { id: 1, col1: "Hello", col2: "World" },
+  { id: 2, col1: "XGrid", col2: "is Awesome" },
+  { id: 3, col1: "Material-UI", col2: "is Amazing" },
+  { id: 4, col1: "Hello", col2: "World" },
+  { id: 5, col1: "XGrid", col2: "is Awesome" },
+  { id: 6, col1: "Material-UI", col2: "is Amazing" }
+];
+
+const columns = [
+  { field: "id" },
+  { field: "col1", headerName: "Column 1", width: 150 },
+  { field: "col2", headerName: "Column 2", width: 150 }
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DataGridPremium rows={rows} columns={columns} />
     </div>
   );
 }
